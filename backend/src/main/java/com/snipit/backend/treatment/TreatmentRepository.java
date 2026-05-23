ab66aee1342b32fb19ed8fb85e3b0ee2a4921994
justkinou <justkinou@proton.me>
2026-05-23 22:49:27 +0200

    add treatment entity and repository

---
+++ backend/src/main/java/com/snipit/backend/treatment/TreatmentRepository.java
@@ -0,0 +1,8 @@
+package com.snipit.backend.treatment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TreatmentRepository extends JpaRepository<Treatment, Integer> {
+}
