ba0e225caa4195c6d855bdab47b9d49c8b34f4d3
Mikita Dzeviatau <9mikita@gmail.com>
2026-05-30 16:04:19 +0200

    Move authentication config from SecurityConfiguration.java to AuthenticationConfiguration.java

--- backend/src/main/java/com/snipit/backend/SecurityConfiguration.java
+++ backend/src/main/java/com/snipit/backend/SecurityConfiguration.java
@@ -5,21 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
-
-import com.snipit.backend.user.UserRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-        private final UserRepository repository;
-
-        public SecurityConfiguration(UserRepository repository) {
-                this.repository = repository;
-        }
-
         @Bean
         SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
                 return httpSecurity
@@ -30,16 +20,5 @@ public class SecurityConfiguration {
                                                 .permitAll()
                                                 .anyRequest().authenticated())
                                 .build();
-        }
-
-        @Bean
-        public UserDetailsService userDetailsService() {
-                return username -> repository.findByEmail(username)
-                        .map(user -> org.springframework.security.core.userdetails.User.builder()
-                                .username(user.getEmail())
-                                .password(user.getPasswordHash())
-                                .roles(Boolean.TRUE.equals(user.getIsAdmin()) ? "ADMIN" : "USER")
-                                .build())
-                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
         }
 }
---
+++ backend/src/main/java/com/snipit/backend/config/AuthenticationConfiguration.java
@@ -0,0 +1,45 @@
+package com.snipit.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.snipit.backend.user.UserRepository;
+
+@Configuration
+public class AuthenticationConfiguration {
+	private final UserRepository repository;
+
+	public AuthenticationConfiguration(UserRepository repository) {
+		this.repository = repository;
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return username -> repository.findByEmail(username)
+				.map(user -> org.springframework.security.core.userdetails.User.builder()
+						.username(user.getEmail())
+						.password(user.getPasswordHash())
+						.roles(Boolean.TRUE.equals(user.getIsAdmin()) ? "ADMIN" : "USER")
+						.build())
+				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+	}
+
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService());
+		authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+		return authenticationProvider;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
