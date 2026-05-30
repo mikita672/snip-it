524f010a601d8781ae543f339c67083f12ac2f71
Mikita Dzeviatau <9mikita@gmail.com>
2026-05-30 15:02:18 +0200

    Add JwtService class implementation

--- backend/src/main/java/com/snipit/backend/config/JwtService.java
+++ backend/src/main/java/com/snipit/backend/config/JwtService.java
@@ -1,12 +1,32 @@
 package com.snipit.backend.config;
 
+import javax.crypto.SecretKey;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY = "fad111bde61a46f56ab0173445020aed83e5560cbda515e10605493cf93ffa54";
 
     public String extractUsername(String jwt) {
-        return null;
+        return extractAllClaims(jwt).getSubject();
     }
 
+    private Claims extractAllClaims(String jwt) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(jwt)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
