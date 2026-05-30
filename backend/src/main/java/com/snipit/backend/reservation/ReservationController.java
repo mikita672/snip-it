d023cdc48d5ed5e749386834079f32c21710d0c4
B1ona4y <eu.oleynich@gmail.com>
2026-05-30 21:13:25 +0200

    feat: add ReservationController for managing reservation endpoints

---
+++ backend/src/main/java/com/snipit/backend/reservation/ReservationController.java
@@ -0,0 +1,44 @@
+package com.snipit.backend.reservation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservation")
+@Tag(name = "Reservation", description = "Reservation management endpoints")
+public class ReservationController {
+    private final ReservationService reservationService;
+    
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+    
+    @GetMapping
+    public List<ReservationResponseDTO> getAll(){
+        return reservationService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ReservationResponseDTO getById(@PathVariable Integer id) {
+        return reservationService.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationResponseDTO> create(@RequestBody ReservationRequestDTO room) {
+        return new ResponseEntity<>(reservationService.create(room), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ReservationResponseDTO update(@PathVariable Integer id, @RequestBody ReservationRequestDTO room) {
+        return reservationService.update(id, room);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        reservationService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+    
+}
