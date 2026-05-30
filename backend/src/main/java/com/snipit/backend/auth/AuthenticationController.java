212eb1af8e0b75186500e261d0a420c98734e301
Mikita Dzeviatau <9mikita@gmail.com>
2026-05-30 17:19:28 +0200

    Refactor cookie handling in authentication methods and implement logout functionality

--- backend/src/main/java/com/snipit/backend/auth/AuthenticationController.java
+++ backend/src/main/java/com/snipit/backend/auth/AuthenticationController.java
@@ -24,27 +24,41 @@ public class AuthenticationController {
     public ResponseEntity<AuthenticationResponse> register(@Valid @RequestBody RegisterRequestDTO request) {
         String jwt = service.register(request);
         ResponseCookie cookie = ResponseCookie.from(JWT_COOKIE_NAME, jwt)
-            .httpOnly(true)
-            .secure(false)
-            .path("/")
-            .sameSite("Strict")
-            .build();
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .sameSite("Strict")
+                .build();
         return ResponseEntity.ok()
-            .header(HttpHeaders.SET_COOKIE, cookie.toString())
-            .body(new AuthenticationResponse());
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new AuthenticationResponse());
     }
 
     @PostMapping("/sign-in")
     public ResponseEntity<AuthenticationResponse> authenticate(@Valid @RequestBody AuthenticateRequestDTO request) {
         String jwt = service.authenticate(request);
         ResponseCookie cookie = ResponseCookie.from(JWT_COOKIE_NAME, jwt)
-            .httpOnly(true)
-            .secure(false)
-            .path("/")
-            .sameSite("Strict")
-            .build();
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .sameSite("Strict")
+                .build();
         return ResponseEntity.ok()
-            .header(HttpHeaders.SET_COOKIE, cookie.toString())
-            .body(new AuthenticationResponse());
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new AuthenticationResponse());
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout() {
+        ResponseCookie cookie = ResponseCookie.from(JWT_COOKIE_NAME, "")
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .sameSite("Strict")
+                .maxAge(0)
+                .build();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 }
