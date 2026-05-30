3439e934ab0ed13bc2484e6cd91f99baab018fdf
Mikita Dzeviatau <9mikita@gmail.com>
2026-05-30 15:30:35 +0200

    Add userDetailsService implementation

--- backend/src/main/java/com/snipit/backend/SecurityConfiguration.java
+++ backend/src/main/java/com/snipit/backend/SecurityConfiguration.java
@@ -5,20 +5,41 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.snipit.backend.user.UserRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
-        return httpSecurity
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers(
-                                "/actuator/health")
-                        .permitAll()
-                        .anyRequest().authenticated())
-                .build();
-    }
+        private final UserRepository repository;
+
+        public SecurityConfiguration(UserRepository repository) {
+                this.repository = repository;
+        }
+
+        @Bean
+        SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+                return httpSecurity
+                                .csrf(AbstractHttpConfigurer::disable)
+                                .authorizeHttpRequests((authorize) -> authorize
+                                                .requestMatchers(
+                                                                "/actuator/health")
+                                                .permitAll()
+                                                .anyRequest().authenticated())
+                                .build();
+        }
+
+        @Bean
+        public UserDetailsService userDetailsService() {
+                return username -> repository.findByEmail(username)
+                        .map(user -> org.springframework.security.core.userdetails.User.builder()
+                                .username(user.getEmail())
+                                .password(user.getPasswordHash())
+                                .roles(Boolean.TRUE.equals(user.getIsAdmin()) ? "ADMIN" : "USER")
+                                .build())
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        }
 }
