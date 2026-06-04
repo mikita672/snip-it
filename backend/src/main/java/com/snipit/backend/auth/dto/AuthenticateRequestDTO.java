ad1628b9343819a9de75f420a88fd66376adad65
Mikita Dzeviatau <9mikita@gmail.com>
2026-06-04 21:15:59 +0200

    refactor AuthenticateRequestDTO.java to use Lombok adnotations

--- backend/src/main/java/com/snipit/backend/auth/dto/AuthenticateRequestDTO.java
+++ backend/src/main/java/com/snipit/backend/auth/dto/AuthenticateRequestDTO.java
@@ -2,8 +2,18 @@ package com.snipit.backend.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public record AuthenticateRequestDTO(
-                @NotBlank @Email String email,
-                @NotBlank String password) {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticateRequestDTO {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
 }
